cca50dd47540901148cd411c80c971ffed38c489
SuperMT <supertempler@gmail.com>
2022-11-04 23:33:27 +0800

    add the create command

--- tools/try_cluster/two_server_cluster.sh
+++ tools/try_cluster/two_server_cluster.sh
@@ -21,19 +21,41 @@
 BIN_PATH="../../cmake-build-debug/"
 HOST=0.0.0.0
 PORT=30000
-NODES=2
+START_NODES=2
+EMPTY_NODES=3
 
 # Computed vars
-ENDPORT=$((PORT + NODES))
+ENDPORT=$((PORT + START_NODES))
+TOTALPORT=$((ENDPORT + EMPTY_NODES))
 
 slots_range=("0-5460" "5461-16383")
 node_id=("kvrockskvrockskvrockskvrockskvrocksnode1"
   "kvrockskvrockskvrockskvrockskvrocksnode2")
 
 if [ "$1" == "start" ]; then
+
+  index=0
+  echo $TOTALPORT
+  while [ $((PORT < TOTALPORT)) != "0" ]; do
+    PORT=$((PORT + 1))
+    echo "Starting $PORT"
+    mkdir node_${PORT}
+    conf_file="node_${PORT}.conf"
+    cp ./default.conf ${conf_file}
+    sed -i.bak "s|pidfile.*|pidfile  node_${PORT}.pid|g" ${conf_file} && rm ${conf_file}.bak
+    sed -i.bak "s|port.*|port ${PORT}|g" ${conf_file} && rm ${conf_file}.bak
+    sed -i.bak "s|dir.*|dir "node_${PORT}"|g" ${conf_file} && rm ${conf_file}.bak
+    nohup $BIN_PATH/kvrocks -c ${conf_file} &>nohup$index.out &
+    index=$((index + 1))
+  done
+  exit 0
+fi
+
+if [ "$1" == "create" ]; then
+
   index=0
   cluster_nodes=""
-  while [ $((index < NODES)) != "0" ]; do
+  while [ $((index < START_NODES)) != "0" ]; do
     slotindex=$index
     slots=${slots_range[$slotindex]}
     echo $slots
@@ -44,49 +66,37 @@ if [ "$1" == "start" ]; then
 
   index=0
   while [ $((PORT < ENDPORT)) != "0" ]; do
-    PORT=$((PORT + 1))
-    echo "Starting $PORT"
-    mkdir node_${PORT}
-    conf_file="node_${PORT}.conf"
-    cp ./default.conf ${conf_file}
-    sed -i.bak "s|pidfile.*|pidfile  node_${PORT}.pid|g" ${conf_file} && rm ${conf_file}.bak
-    sed -i.bak "s|port.*|port ${PORT}|g" ${conf_file} && rm ${conf_file}.bak
-    sed -i.bak "s|dir.*|dir "node_${PORT}"|g" ${conf_file} && rm ${conf_file}.bak
-    nohup $BIN_PATH/kvrocks -c ${conf_file} &>nohup$index.out &
-    sleep 2
+    PORT=$((PORT+1))
     redis-cli -h 127.0.0.1 -p $PORT clusterx setnodes "${cluster_nodes}" 1
     redis-cli -h 127.0.0.1 -p $PORT clusterx setnodeid ${node_id[$index]}
     index=$((index + 1))
-  done
-
-  index=0
-
+  done 
   exit 0
 fi
 
-if [ "$1" == "migrate" ]; then
-  SLOTS=5460
-  MIGRATE_ID=0
-  while [ $((MIGRATE < SLOTS)) != "0" ]; do
-    MIGRATE_ID=$((MIGRATE_ID + 1))
-    echo "Migrate $MIGRATE_ID"
-    VERSION_NUM=$((MIGRATE_ID + 1))
-    redis-cli -h 127.0.0.1 -p 30001 clusterx migrate $MIGRATE_ID kvrockskvrockskvrockskvrockskvrocksnode2
-    redis-cli -h 127.0.0.1 -p 30001 clusterx setslot $MIGRATE_ID NODE kvrockskvrockskvrockskvrockskvrocksnode2 $VERSION_NUM
-    redis-cli -h 127.0.0.1 -p 30002 clusterx setslot $MIGRATE_ID NODE kvrockskvrockskvrockskvrockskvrocksnode2 $VERSION_NUM
-  done
-  exit 0
-fi
+# if [ "$1" == "migrate" ]; then
+#   SLOTS=5460
+#   MIGRATE_ID=0
+#   while [ $((MIGRATE < SLOTS)) != "0" ]; do
+#     MIGRATE_ID=$((MIGRATE_ID + 1))
+#     echo "Migrate $MIGRATE_ID"
+#     VERSION_NUM=$((MIGRATE_ID + 1))
+#     redis-cli -h 127.0.0.1 -p 30001 clusterx migrate $MIGRATE_ID kvrockskvrockskvrockskvrockskvrocksnode2
+#     redis-cli -h 127.0.0.1 -p 30001 clusterx setslot $MIGRATE_ID NODE kvrockskvrockskvrockskvrockskvrocksnode2 $VERSION_NUM
+#     redis-cli -h 127.0.0.1 -p 30002 clusterx setslot $MIGRATE_ID NODE kvrockskvrockskvrockskvrockskvrocksnode2 $VERSION_NUM
+#   done
+#   exit 0
+# fi
 
 if [ "$1" == "stop" ]; then
-  while [ $((PORT < ENDPORT)) != "0" ]; do
+  while [ $((PORT < TOTALPORT)) != "0" ]; do
     PORT=$((PORT + 1))
     echo "Stopping $PORT"
     redis-cli -h 127.0.0.1 -p $PORT shutdown
   done
   rm -r ./node_*
   rm ./nohup*
-  hadoop fs -rm -r -skipTrash /user/supermt
+#   hadoop fs -rm -r -skipTrash /user/supermt
   exit 0
 fi
 
@@ -101,7 +111,7 @@ if [ "$1" == "watch" ]; then
   exit 0
 fi
 
-echo "Usage: $0 [start|stop|watch|migrate]"
+echo "Usage: $0 [start|stop|watch|migrate|create]"
 echo "start       -- Launch Redis Cluster instances."
 echo "stop        -- Stop Redis Cluster instances."
 echo "watch       -- Show CLUSTER NODES output (first 30 lines) of first node."
